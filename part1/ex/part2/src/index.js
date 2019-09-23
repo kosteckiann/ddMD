03bace282d401f27c42eb5e243d6b44dc0419d78
kosteckiann <kostecka@oregonstate.edu>
2019-09-22 19:51:14 -0700

    1.6

---
+++ part1/ex/part2/src/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={handleGoodClick}>
+        good
+      </button>
+      <button onClick={handleNeutralClick}>
+        neutral
+      </button>
+      <button onClick={handleBadClick}>
+        bad
+      </button>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      
+      
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
